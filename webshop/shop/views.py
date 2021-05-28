29ecdae127f567cf3bd100b8e2e6e66f11149cff
miikisalminen <miika.salminen11@outlook.com>
2021-05-28 09:31:47 +0300

    Finalized signup/login

--- webshop/shop/views.py
+++ webshop/shop/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from .models import *
 from rest_framework.response import Response
 from .serializer import *
+from .forms import AccountRegisterForm
 
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
@@ -12,15 +13,19 @@ from django.shortcuts import render, redirect
 
 from rest_framework import generics
 
+
+# Landing page
 def home(request):
     return render(request, 'templates/shop/main.html')
 
+# Shop page
 def shop(request):
     return render(request, 'build/index.html')
 
+# Account creation (Using Django's premade form)
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = AccountRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -29,11 +34,18 @@ def signup(request):
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
-    return render(request, 'templates/shop/signup.html', {'form': form})
+        form = AccountRegisterForm()
+    return render(request, 'templates/registration/signup.html', {'form': form})
 
+# --- API ENDPOINTS ---
+
+# Fetch userdata from the REST API
 class UserView(APIView):
     serializer_class = UserSerializer
 
+    # Get username, if the requesting user is not logged in, return "Guest"
     def get(self, request):
-        return Response(request.user.username)
+        if(request.user.is_authenticated):
+            return Response(request.user.username)
+        else:
+            return Response("Guest")
